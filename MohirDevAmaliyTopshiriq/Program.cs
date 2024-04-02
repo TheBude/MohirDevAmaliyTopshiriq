3f18baf64240fd87a5b44dff7c128145e7a6ac85
TheBude <husaenbobmuratov192@gmail.com>
2024-04-02 16:00:56 +0500

    Add project files.

---
+++ MohirDevAmaliyTopshiriq/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("string kiriting: ");
+
+string str = Console.ReadLine();
+
+Console.Write("Soni Kiriting: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > str.Length)
+{
+    Console.WriteLine(str.ToUpper());
+}
+else
+{
+    Console.WriteLine(str.ToLower());
+}
