e9820145c8986e09f28caa6f32380b2a93f3397e
TheBude <husaenbobmuratov192@gmail.com>
2024-04-04 21:47:06 +0500

    3-misol

--- MohirDevAmaliyTopshiriq/Program.cs
+++ MohirDevAmaliyTopshiriq/Program.cs
@@ -1,25 +1,18 @@
-﻿int x = 15;
-int y = 10;
+﻿//3-misol hafta kunini tarjima 
+Console.WriteLine("Assalomu allaykum men hafta kunlarini ingliz tiliga tarjima qiluvchi dasturman");
 
-if (x > y)
+Console.Write("Iltimos hafta kunini birinchi harfini katta bilan bo'lsin\nQaysi hafta kunini tarjima qilish kerak.\nKirting: ");
+string weekday = Console.ReadLine();
+
+string result = weekday switch
 {
-    Console.WriteLine("x is greather than y");
-}
-else
-{
-    if (x < y)
-    {
-        Console.WriteLine("x is less than y");
-    }
-    else
-    {
-        if (x == y)
-        {
-            Console.WriteLine("x is equal to y");
-        }
-        else
-        {
-            Console.WriteLine("x and y are not comprable");
-        }
-    }
-}
+    "Dushanba" => "Monday",
+    "Seshanba" => "Tuesday",
+    "Chorshanba" => "Wednesday",
+    "Payshanba" => "Thursday",
+    "Juma" => "Friday",
+    "Shanba" => "Saturday",
+    "Yakshanba" => "Sunday",
+    _ => "Iltimos hafta kunini tug'ri kiriting"
+};
+Console.WriteLine(result);
